fee7f6af2810ab6fad8a399aa55ab7290c825fa5
Asmanova <l.vdovenko@mail.ru>
2016-09-18 11:04:02 +0300

    fixed

--- hw1.0/js/script.js
+++ hw1.0/js/script.js
@@ -1,15 +1,25 @@
-var number = prompt ('Введите число');
-var degree = prompt ('Введите степень');
 var result;
 
-function pow(num, degr) {
+function pow() {
+  var num = prompt ('Введите число');
+  while (num == '') {
+    alert ('Число не введено');
+    num = prompt ('Введите число');
+  }
+
+  var degr = prompt ('Введите степень');
+  while (degr == '') {
+    alert ('Сетепень не введена');
+    degr = prompt ('Введите степень');
+  }
+
   var res = num;
 
   while (degr % 1 != 0) {
     alert ('Число в степени должно быть целым');
-    degr = prompt ('Введите число');
+    degr = prompt ('Введите степень');
   }
-  
+
   if (degr == 0) {
     res = 1;
   } else if (degr < 0) {
@@ -26,5 +36,5 @@ function pow(num, degr) {
   return res;
 }
 
-result = pow(number, degree);
+result = pow();
 console.log('Ответ:', result);
